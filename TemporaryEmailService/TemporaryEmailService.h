ce3d0934b3a32ca129d774c42ccb937605e376c7
Gerson Arbigaus <gerson87@gmail.com>
2023-04-17 09:31:02 -0300

    Initial Commit

---
+++ TemporaryEmailService/TemporaryEmailService.h
@@ -0,0 +1,18 @@
+//
+//  TemporaryEmailService.h
+//  TemporaryEmailService
+//
+//  Created by Gerson Arbigaus on 17/04/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TemporaryEmailService.
+FOUNDATION_EXPORT double TemporaryEmailServiceVersionNumber;
+
+//! Project version string for TemporaryEmailService.
+FOUNDATION_EXPORT const unsigned char TemporaryEmailServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TemporaryEmailService/PublicHeader.h>
+
+
